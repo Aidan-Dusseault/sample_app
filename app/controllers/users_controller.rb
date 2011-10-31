bd591ef2c31dcd73257c8fe46d1f43d1d615b904
unknown <Aidan@.(none)>
2011-10-31 13:05:53 -0400

    User model with passwords and user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
-  def new
-	@user = User.find(params[:id])
-  end
+	def show
+		@user = User.find(params[:id])
+		@title = @user.name
+	end
 
-  def new
-	@title = "Sign up"
-  end
+	def new
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@title = "Sign up"
+	end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,16 +3,32 @@ require 'spec_helper'
 	describe UsersController do
 	render_views
 	
-  describe "GET 'new'" do
-    it "should be successful" do
-      get 'new'
-      response.should be_success
-    end
+	describe "GET 'show'" do
+		it "should have the right title" do
+			get :show, :id => @user
+			response.should have_selector("title", :content => @user.name)
+		end
+		
+		it "should include the user's name" do
+			get :show, :id => @user
+			response.should have_selector("h1", :content => @user.name)
+			
+			it "should have a profile image" do
+				get :show, :id => @user
+				response.should have_selector("h1>img", :class => "gravatar")
+			end
+		end
 	
-	it "should have the right title" do
-		get 'new'
-		response.should have_selector("title", :content => "Sign up")
+	describe "GET 'new'" do
+		it "should be successful" do
+			get 'new'
+			response.should be_success
+		end
+	
+		it "should have the right title" do
+			get 'new'
+			response.should have_selector("title", :content => "Sign up")
+		end
 	end
-  end
 
 end
