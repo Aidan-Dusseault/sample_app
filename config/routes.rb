dd181bc3ce69aff6b5f6a4f8f52f889544cc6cf2
unknown <Aidan@.(none)>
2011-11-01 13:59:09 -0400

    User signup complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ SampleApp::Application.routes.draw do
   resources :users
 
   match '/signup', :to => 'users#new'
-  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
